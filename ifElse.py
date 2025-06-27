e8138247dd2e83c857a3acdd61563a9b227ccd80
Wilton24 <cbasher24@gmail.com>
2025-06-27 20:42:05 +0800

    for loop

--- ifElse.py
+++ ifElse.py
@@ -8,8 +8,21 @@
 #     print("Your number is as big as yo Mama!")
 
 
-temperature = input("Input a temperature: ")
-message = "Temperature is too hight" if int(
-    temperature) > 99 else "Temperature is normal"
+# temperature = input("Input a temperature: ")
+# message = "Temperature is too hight" if int(
+#     temperature) > 99 else "Temperature is normal"
 
-print(message)
+# print(message)
+
+
+# for c in range(3):
+#     print(c)
+
+
+target = 420
+
+for c in range(1, 500):
+    print(c)
+    if c == target:
+        print("Happy Weed day")
+        break
