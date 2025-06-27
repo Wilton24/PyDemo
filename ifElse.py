c3ece98624fa19f64c19a56772916335591a6edf
Wilton24 <cbasher24@gmail.com>
2025-06-27 11:11:17 +0800

    ternary operator

--- ifElse.py
+++ ifElse.py
@@ -1,8 +1,15 @@
-numberInputted = input("Input a number: ")
+# numberInputted = input("Input a number: ")
 
-if int(numberInputted) <= 10:
-    print('The number you inputted is SMOL')
-elif int(numberInputted) > 10 and int(numberInputted) <= 50:
-    print("your number is MID")
-else:
-    print("Your number is as big as yo Mama!")
+# if int(numberInputted) <= 10:
+#     print('The number you inputted is SMOL')
+# elif int(numberInputted) > 10 and int(numberInputted) <= 50:
+#     print("your number is MID")
+# else:
+#     print("Your number is as big as yo Mama!")
+
+
+temperature = input("Input a temperature: ")
+message = "Temperature is too hight" if int(
+    temperature) > 99 else "Temperature is normal"
+
+print(message)
