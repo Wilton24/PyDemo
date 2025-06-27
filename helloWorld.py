e15b00d8aedc3c0204722b9ad51da8ebba280a3d
Wilton24 <cbasher24@gmail.com>
2025-06-27 10:09:26 +0800

    Modulus and input

--- helloWorld.py
+++ helloWorld.py
@@ -1,14 +1,20 @@
-a = 10
-b = 20
+# a = 10
+# b = 20
 
-print('Hello World')
-print(a+b)
+# print('Hello World')
+# print(a+b)
 
+# def aB(a, b):
+#     return a+b
+# print(aB(500, 1))
 
-def aB(a, b):
-    return a+b
+# unitPrice = 103
 
+# x = 2
+# input("Enter a number to be incremented by 1: ")
 
-print(aB(500, 1))
+# y = x + 1
+# print(y)
+# modulus operator:
 
-unitPrice = 103
+print(20 % 3)
