5ade233ca6e2eb1fb99b6481eb8d1ff147d35c72
Wilton24 <cbasher24@gmail.com>
2025-06-28 16:02:05 +0800

    functions

---
+++ functions.py
@@ -0,0 +1,5 @@
+def greet(name, message="What's up"):
+    print(f"Hello {name}, {message}")
+
+
+greet("Michael Almano")
--- ifElse.py
+++ ifElse.py
@@ -28,13 +28,36 @@
 #         break
 
 
-password = ""
+# password = ""
 
-while password != "pass123":
-    password = input('Input password: ')
-    if password == "pass123":
-        break
-    print("Wrong password")
+# while password != "pass123":
+#     password = input('Input password: ')
+#     if password == "pass123":
+#         break
+#     print("Wrong password")
 
 
-print('Logged in!')
+# print('Logged in!')
+
+# target = 2
+
+# while target != 10:
+#     print(target)
+#     target += 2
+#     if target == 10:
+#         print("We have 4 even numbers")
+#         break
+
+# for c in range(2, 20, 2):
+#     if (c < 10):
+#         print(c)
+#     elif (c == 10):
+#         print(f"We have {c} even numbers")
+
+# even_numbers = 0
+
+# for c in range(1, 10):
+#     if c % 2 == 0:
+#         even_numbers += 1
+#         print(c)
+# print(f"we have {even_numbers} even numbers")
